44e70b24983a18bffdb4341d751151c84084bea9
AnaNedyalkova <adnedyalkova@gmail.com>
2025-03-04 20:41:56 +0200

    changes

--- JS-Fundamentals/08-Associative-Arrays/P05-cardGames.js
+++ JS-Fundamentals/08-Associative-Arrays/P05-cardGames.js
@@ -1,4 +1,35 @@
-function cardGames() {
+function cardGames(input) {
+
+    let playersNcards = {};
+    let cardPower = { '2' : 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+        '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14,}
+    let cardType = { 'S' : 4, 'H': 3, 'D': 2, 'C': 1 }
+
+    for (let string of input) {
+        let [name, cards] = string.split(': ');
+
+        if (!name in playersNcards) {
+            playersNcards[name] = new Set(cards.split(', '));
+        }
+        else {
+
+        }
+
+    }
+
+    console.log(playersNcards)
+
+    // let cardsPerPerson = Object.values(playersNcards);
+
+    // for (card of cardsPerPerson) {
+    //     uniqueCards.add(card);
+    // }
+
+
+
+
+
+
 
 }
 cardGames([
